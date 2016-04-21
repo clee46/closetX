ceaded95f0cff74e07269d624d598ef39d2679d8
Christopher Lee <yensen@gmail.com>
2016-04-21 14:00:37 -0700

    completed login route and checked that user does not already exist

--- routes/auth_routes.js
+++ routes/auth_routes.js
@@ -10,45 +10,56 @@ var authRouter = module.exports = exports = express.Router();
 
 authRouter.post('/signup', jsonParser, (req, res) => {
 
-  // Check that email field was not empty
   if (!(req.body.email || '').length && !emailValidation(req.body.email)) {
     return res.status(400).json({ msg: 'Please Enter an Email' });
   }
 
-  // Check that email field contained an actual email address
   if (!emailValidation(req.body.email)) {
     return res.status(400).json({ msg: 'Please Enter a Valid Email' });
   }
 
-  // Check that username field was not empty
   if (!(req.body.username || '').length) {
     return res.status(400).json({ msg: 'Please Enter a User Name' });
   }
 
-  // Check that password was long enough
   if (!((req.body.password || '').length > 7)) {
     return res.status(400)
       .json({ msg: 'Please Enter a Password Longer Than 7 Characters' });
   }
 
-  // Check that the two passwords matched
   if (!(req.body.password === req.body.confirmpassword)) {
-    return res.status(400).json({ msg: 'Passwords Are Not the Same' });
+    return res.status(400).json({ msg: 'Passwords Do Not Match' });
   }
 
   // Check that the user does not already exist in the database
   User.find({
-    $or: [{ 'username': req.body.username }, { 'email': req.body.email }]
+    $or: [{ 'username': req.body.username },
+    { 'authentication.email': req.body.email }]
   }, (err, data) => {
     if (err) return handleDBError(err, res);
     if (data.length) {
       return res.status(400)
-        .json({ msg: 'User Already Exists! Please Use a Different Username' });
+        .json({ msg: 'User Already Exists' });
     }
     saveUser(req, res);
   });
+
 });
 
 authRouter.get('/login', basicHTTP, (req, res) => {
+  User.findOne({ 'authentication.email': req.basicHTTP.email }, (err, user) => {
 
+    if (err) return handleDBError(err, res);
+
+    if (!user) return res.status(401).json({ msg: 'User Does Not Exist' });
+
+    if (!user.comparePassword(req.basicHTTP.password)) {
+      return res.status(401).json({ msg: 'Incorrect Password' });
+    }
+
+    res.json({
+      msg: 'Logged in successfully!',
+      token: user.generateToken()
+    });
+  });
 });
