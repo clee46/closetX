79966c37d745617554d12a2df0ceaaa9288ef610
Christopher Lee <yensen@gmail.com>
2016-04-26 14:43:59 -0700

    added test folder and update request for user route

--- server.js
+++ server.js
@@ -7,10 +7,12 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/app_dev');
 const authRouter = require(__dirname + '/routes/auth_routes.js');
 const connectionRouter = require(__dirname + '/routes/connection_routes.js');
 const itemRouter = require(__dirname + '/routes/item_routes.js');
+const userRouter = require(__dirname + '/routes/user_routes.js');
 
 app.use('/api', authRouter);
 app.use('/api', connectionRouter);
 app.use('/api', itemRouter);
+app.use('/api', userRouter);
 
 var PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log('Server up on port: ' + PORT));
